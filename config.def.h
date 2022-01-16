134756388105672448c444adb8991077fc0d12b9
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-16 13:20:35 +0100

    Apply quickcancel patch

--- config.def.h
+++ config.def.h
@@ -12,6 +12,9 @@ static const char *colorname[NUMCOLS] = {
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
+/* time in seconds to cancel lock with mouse movement */
+static const int timetocancel = 4;
+
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
 
